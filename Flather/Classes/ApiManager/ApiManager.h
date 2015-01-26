fc258f08925c985d06a25f249b888f12732e0c3b
Said Özcan <saidozcn@gmail.com>
2015-01-26 13:49:04 +0200

    Gitignore update

---
+++ Flather/Classes/ApiManager/ApiManager.h
@@ -0,0 +1,13 @@
+//
+//  ApiManager.h
+//  Flather
+//
+//  Created by Said on 25/01/2015.
+//  Copyright (c) 2015 Tower Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ApiManager : NSObject
+
+@end
---
+++ Flather/Classes/ApiManager/ApiManager.m
@@ -0,0 +1,13 @@
+//
+//  ApiManager.m
+//  Flather
+//
+//  Created by Said on 25/01/2015.
+//  Copyright (c) 2015 Tower Labs. All rights reserved.
+//
+
+#import "ApiManager.h"
+
+@implementation ApiManager
+
+@end
